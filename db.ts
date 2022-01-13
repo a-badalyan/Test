bfd47aa36527197b163c4a9c2100cc6dadacb112
A. Badalyan <timmirs@list.ru>
2022-01-13 16:42:54 +0300

    make a class in db.ts

--- db.ts
+++ db.ts
@@ -2,36 +2,36 @@ import axios from "axios";
 const pgp = require('pg-promise')()
 
 
-type cnType = {
-    host: string,
-    port: number,
-    database: string,
-    user: string,
-    password: string
+export default class WeatherSyncer {
+    cn: object = {
+        host: 'localhost',
+        port: 5432,
+        database: 'weather',        // Наименование БД
+        user: 'artem',              // Имя пользователя СУБД 
+        password: '12345',          // Пароль пользователя СУБД
+    };
+
+    db: object;
+
+    cities: Array<string> = ['Moscow', 'Sankt-Peterburg', 'Kazan', 'Nizhniy Novgorod'];
+
+    constructor() {
+        this.db = pgp(this.cn)
+    }
+    getWeather(db: any = this.db): any {
+        setInterval(() => {
+            for (let city of this.cities) {
+                axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},ru&appid=877b59e2e991f956d0394235b7de8467`)
+                    .then((response: any) => {
+                        db.none('INSERT INTO weather(city, temperature, pressure, humidity, dateandtime) VALUES($1, $2, $3, $4, now())',
+                            [response.data.name, Math.round(response.data.main.temp - 273.15), response.data.main.pressure, response.data.main.humidity]);
+                        console.log('Сведения внесены в базу данных')
+                    })
+                    .catch((error: object) => {
+                        console.error(error)
+                    })
+            }
+        }, 10000)
+    }
+
 }
-
-const cn: cnType = {
-    host: 'localhost',
-    port: 5432,
-    database: 'weather',        // Наименование БД
-    user: 'artem',              // Имя пользователя СУБД 
-    password: '12345',          // Пароль пользователя СУБД
-};
-
-const db = pgp(cn);
-
-
-let citiesList: Array<string> = ['Moscow', 'Sankt-Peterburg', 'Kazan', 'Nizhniy Novgorod']
-
-
-setInterval(()=> {for (let city of citiesList) {
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},ru&appid=877b59e2e991f956d0394235b7de8467`)
-    .then((response:any)=>{
-        db.none('INSERT INTO weather(city, temperature, pressure, humidity, dateandtime) VALUES($1, $2, $3, $4, now())',  
-        [response.data.name, Math.round(response.data.main.temp - 273.15), response.data.main.pressure, response.data.main.humidity]);
-        console.log('Сведения внесены в базу данных')
-    })
-    .catch((error:object)=>{
-        console.error(error)
-    })
-}}, 10000)
--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import p_json from './package.json'
 import express from 'express';
+import WeatherSyncer from './db';
 
 
 const app = express();
 const PORT = 8000;
 
-app.get('/', (req: any,res: { send: (arg0: string) => any; }) => res.send('Express + TypeScript Server'));
-app.get('/healthz', (req:any, res: { send: (arg0: string) => any; }) => res.send(`Версия проекта - ${p_json.version}`))
+app.get('/', (req: any, res: { send: (arg0: string) => any; }) => res.send('Express + TypeScript Server'));
+app.get('/healthz', (req: any, res: { send: (arg0: string) => any; }) => res.send(`Product version - ${p_json.version}`))
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
+  new WeatherSyncer().getWeather()
+
 });
